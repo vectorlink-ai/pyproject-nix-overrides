2e79fb1b4d80369a090307b02be88713ca8f8840
Maren van Otterdijk <maren@vectorlink.ai>
2025-02-20 12:23:25 +0100

    ignore cuda for dependencies (they should be coming from torch)

--- cuda.nix
+++ cuda.nix
@@ -1,24 +1,34 @@
 pkgs:
-let addCuda = package: package.overrideAttrs (p: {
-      buildInputs = p.buildInputs ++ (with pkgs.cudaPackages; [
-        cudatoolkit
-        cudnn
-        cuda_cudart
-        nccl
-      ]);
-      postInstall = ''
+let
+  addCuda = package: package.overrideAttrs (p: {
+    buildInputs = p.buildInputs ++ (with pkgs.cudaPackages; [
+      cudatoolkit
+      cudnn
+      cuda_cudart
+      nccl
+    ]);
+    postInstall = ''
 rm -f $out/lib/python3.12/site-packages/nvidia/__pycache__/__init__.cpython-312.pyc
 '';
-    });
+  });
+  ignoreCuda = package: package.overrideAttrs (p: {
+    autoPatchelfIgnoreMissingDeps = [ "libcuda.so.1" "libcudart.so.1" ];
+  });
 
-    addCudaTo = packageSet: names: builtins.listToAttrs (map
-      (name: { inherit name;
-               value = addCuda packageSet.${name};
-             })
-      names
-    );
+  addCudaTo = packageSet: names: builtins.listToAttrs (map
+    (name: { inherit name;
+             value = addCuda packageSet.${name};
+           })
+    names
+  );
+  ignoreCudaFor = packageSet: names: builtins.listToAttrs (map
+    (name: { inherit name;
+             value = ignoreCuda packageSet.${name};
+           })
+    names
+  );
 in
-final: prev: addCudaTo prev [
+final: prev: ignoreCudaFor prev [
   "nvidia-cusolver-cu12"
   "nvidia-cusparse-cu12"
   "nvidia-cublas-cu12"
